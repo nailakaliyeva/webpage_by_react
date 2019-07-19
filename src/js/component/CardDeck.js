13e727b379613762feb366971fb38e7d66349ea0
Naila <nailakaliyeva@gmail.com>
2019-07-19 19:02:03 +0000

    replaced array of links with objects because we eventually will need to give it another title and description

--- src/js/component/CardDeck.js
+++ src/js/component/CardDeck.js
@@ -1,31 +1,47 @@
 import React from "react";
 
-//create your first component
 export default class CardDeck extends React.Component {
 	render() {
-		let pictures = [
-			"http://lorempixel.com/output/nature-q-c-640-480-7.jpg",
-			"http://lorempixel.com/output/nightlife-q-c-640-480-6.jpg",
-			"http://lorempixel.com/output/cats-q-c-640-480-3.jpg"
+		let arrayOfObjects = [
+			{
+				imageUrl: "http:lorempixel.com/output/nature-q-c-640-480-7.jpg",
+				cardTitle: "A beautiful flower",
+				cardDescription:
+					"This is some dummy text but the flower IS actually beautiful just like Me"
+			},
+
+			{
+				imageUrl:
+					"http:lorempixel.com/output/nightlife-q-c-640-480-6.jpg",
+				cardTitle: "The bar",
+				cardDescription:
+					"This bar is advertising cheap ass Vodka. You might not notice you're drinking cheap or expensive vodka at the moment, but you will definitely feel it next morning"
+			},
+
+			{
+				imageUrl: "http:lorempixel.com/output/cats-q-c-640-480-3.jpg",
+				cardTitle: "Grumpy Cat",
+				cardDescription:
+					"This cat is still cute regardless how mad he looks"
+			}
 		];
 		return (
 			<div id="parent">
 				<div className="card-deck">
-					{pictures.map((item, index) => {
+					{arrayOfObjects.map((item, index) => {
 						return (
 							<div className="card" key={index}>
 								<img
-									src={item}
+									src={item.imageUrl}
 									className="card-img-top"
 									alt="..."
 								/>
 								<div className="card-body">
-									<h5 className="card-title">Card title</h5>
+									<h5 className="card-title">
+										{item.cardTitle}
+									</h5>
 									<p className="card-text">
-										This is a longer card with supporting
-										text below as a natural lead-in to
-										additional content. This content is a
-										little bit longer.
+										{item.cardDescription}
 									</p>
 									<p className="card-text">
 										<small className="text-muted">
